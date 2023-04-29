99f3918a90f75009198de8db21f84ab30154d5ea
marwan123-web <marawansalman98@gmail.com>
2023-04-29 18:08:37 +0300

    finished ScrollToTop comp

---
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import "./ScrollToTop.scss";
+import IconImg from "../../components/Shared/IconImg";
+
+import RedirectTop from "../../assets/images/redirect-top.svg";
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () =>
+      window.pageYOffset > 500 ? setIsVisible(true) : setIsVisible(false);
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return isVisible ? (
+    <div className="scroll-top">
+      <a href="#top">
+        <IconImg src={RedirectTop} alt={"RedirectTop"} />
+      </a>
+    </div>
+  ) : null;
+};
+
+export default ScrollToTop;
